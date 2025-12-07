4ccaf02c7046a1fcdf8c86bf3cbacbf1afc8d5fd
undefined <m.sahanda@gmail.com>
2025-12-07 20:33:31 +0200

    Homework for lesson 21 And some improvements in side menu spec

--- lesson18/tests/side-menu.spec.ts
+++ lesson18/tests/side-menu.spec.ts
@@ -12,18 +12,14 @@ test.describe('side menu', () => {
         await helpers.checkItemsVisibleWithLen(sideMenu.menuItems, 5);
     });
 
-    test('click on menu menu and page should be changed', async ({ sideMenu, page, footer }) => {
+    test('click on menu and page should be changed', async ({ sideMenu, footer }) => {
         await sideMenu.menuItems.nth(1).click();
-        await expect(sideMenu.menuBlock).toBeVisible();
-        await helpers.checkItemsVisibleWithLen(sideMenu.menuItems, 5);
-        await expect(page).not.toHaveURL(/\/wishlists$/);
+        await sideMenu.assertMenuItemsCount();
         await footer.assertFooterItemsVisible();
     });
-    test('click on another menu item and utl should be changed', async ({ sideMenu, page, footer }) => {
+    test('click on another menu item and url should be changed', async ({ sideMenu, footer }) => {
         await sideMenu.menuItems.nth(3).click();
-        await expect(sideMenu.menuBlock).toBeVisible();
-        await helpers.checkItemsVisibleWithLen(sideMenu.menuItems, 5);
-        await expect(page).not.toHaveURL(/\/wishlists$/);
+        await sideMenu.assertMenuItemsCount();
         await footer.assertFooterItemsVisible();
     });
 });
